2b0f8c3bf7148bc42eaa4de19b4281a4ccbcd36e
carlos Montes <carlos.montesdeoca.h@gmail.com>
2018-04-01 23:34:34 -0500

    Ajustes de scraping y consulta sin un formato claro

--- functions/index.js
+++ functions/index.js
@@ -1,54 +1,83 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase');
 var express = require('express');
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.holamundo = functions.https.onRequest((req, res) => {
+const appFirebase = firebase.initializeApp({
+    apiKey: "AIzaSyAH1o7sEzotd6rjzOABjcNEYtdDZW3UfT8",
+    authDomain: "scraping-6960e.firebaseapp.com",
+    databaseURL: "https://scraping-6960e.firebaseio.com",
+    projectId: "scraping-6960e",
+    storageBucket: "scraping-6960e.appspot.com",
+    messagingSenderId: "381148053741"    
+});
 
-    url = 'http://www.snopes.com/category/facts/';
-    respuesta="Fin de Ejecucion";
+ exports.scrapingfifa = functions.https.onRequest((req, res) => {
+
+    var url = 'http://www.fifa.com/worldcup/matches/index.html';
+    var json = { partidos: []};
+    var respuesta = "Exito";
  
     request(url, function(error, response, html) {
         if(!error) {
             var $ = cheerio.load(html);
-            var title;
-            var counter = 0;
-            var story = {
-                title : "",
-            };
-            var json = { articles: []};
+            var pais1;
+            var pais2;
+            var fecha;
+            var etapa;
+            var resultado;
+            var penales;
+            var estatus;
             console.log("Ingreso a scrapear");
-            $('h2.title').each(function() {
-                story = {title: ""};
-                var data = $(this);
-                title = data.text();
-                story.title = title
-                json.articles.push(story);
-                console.log("Capturando Datos... " + data.text());
+            var storpartido = {
+                home : "",
+                visita : "",
+                etapa : "",
+                fecha : "",
+                estatus : "",
+                resultado : "",
+                penales : "",
+            };        
+            $('div.col-xs-12.clear-grid').each(function() {
+    
+                var data = $(this).children();
+                pais1 = data.children(".mu-m").children(".t.home").children(".t-n").children(".t-nText").text();
+                pais2 = data.children(".mu-m").children(".t.away").children(".t-n").children(".t-nText").text();
+                etapa = data.children(".mu-i").children(".mu-i-group").text();
+                fecha = data.children(".mu-i").children(".mu-i-datetime").text();
+                estatus = data.children(".mu-m").children(".s").children(".s-fixture").children(".s-status-abbr").text();
+                resultado = data.children(".mu-m").children(".s").children(".s-fixture").children(".s-score.s-date-HHmm").children(".s-scoreText").text();
+                penales = data.children(".mu-m").children(".s").children(".s-fixture").children(".mu-reasonwin-abbr").children(".text-reasonwin").text();
+    
+                storpartido = {
+                    home : pais1,
+                    visita : pais2,
+                    etapa : etapa,
+                    fecha : fecha,
+                    estatus : estatus,
+                    resultado : resultado,
+                    penales : penales,
+                };        
+                json.partidos.push(storpartido);
             })
-
-            console.log(JSON.stringify(json));
-
+            //console.log(JSON.stringify(json));
+            var database = firebase.database();         
+            var ref = database.ref("/").set(json);
        }
-
-       
-
-        // To write to the system we will use the built in 'fs' library.
-        // In this example we will pass 3 parameters to the writeFile function
-        // Parameter 1 :  output.json - this is what the created filename will be called
-        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-        // Parameter 3 :  callback function - a callback function to let us know the status of our function
-        /*
-        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-            console.log('File successfully written! - Check your project directory for the output.json file');
-        })
-        */
     })   
 
     res.send(respuesta);
  });
+
+ exports.consultafifa = functions.https.onRequest((req, res) => {
+    var respuesta = "Exito";
+    var ref = firebase.database().ref('/');
+    var partidos;
+
+    ref.on("value", function(snapshot) {
+        partidos = snapshot.val();
+    });  
+    res.send(partidos);
+ });    
